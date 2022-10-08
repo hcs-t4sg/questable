b465b6825a42c3d5c2a6ca36f42f3801f755de46
Alex Estevez <aestevez@college.harvard.edu>
2022-10-08 19:38:34 -0400

    isolate completed-task code

--- src/components/TeacherView.js
+++ src/components/TeacherView.js
@@ -1,13 +1,6 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import TextField from '@mui/material/TextField';
-import * as React from 'react';
-import { addTask} from '../utils/mutations';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -20,14 +13,9 @@ import { collection, onSnapshot, query} from "firebase/firestore";
 import { db } from '../utils/firebase';
 import { denyTask, confirmTask } from '../utils/mutations'
 
-export default function TeacherView({ player, classroom, user }) {
-   const [completedTasks, setCompletedTasks] = useState(null)
+export default function TeacherView({ player, classroom }) {
+   const [completedTasks, setCompletedTasks] = useState([])
    const [playerData, setPlayerData] = useState(null)
-   const [open, setOpen] = useState(false);
-   const [name, setName] = useState("");
-   const [description, setDescription] = useState("");
-   const [reward, setReward] = React.useState(0);
-   const [due, setDue] = React.useState(Date.now);
 
    useEffect(() => {
       // fetch player information
@@ -59,45 +47,7 @@ export default function TeacherView({ player, classroom, user }) {
 
    }, [classroom.id])
 
-   const handleClickOpen = () => {
-      setOpen(true);
-      setName("");
-      setDescription("");
-      setReward(0);
-      setDue(Date.now);
-   };
-
-   const handleClose = () => {
-      setOpen(false);
-   };
-
-   // Mutation handlers
-
-   const handleAdd = () => {
-      const newTask = {
-         name: name,
-         description: description,
-         reward: reward,
-         due: due,
-      };
-      
-      addTask(classroom.id, newTask, user).catch(console.error);
-      handleClose();
-   };
-
-   const openButton =
-      <Button variant="contained" onClick={handleClickOpen}>
-         Add Task
-      </Button>
-
-   const actionButtons =
-      <DialogActions>
-         <Button onClick={handleClose}>Cancel</Button>
-         <Button variant="contained" onClick={handleAdd}>Add Task</Button>
-      </DialogActions>
-
    return (
-   <>
       <Grid container spacing={3}>
          <Grid item xs={12}>
             <Typography variant="h2">{classroom.name}</Typography>
@@ -136,58 +86,5 @@ export default function TeacherView({ player, classroom, user }) {
          </Table>
       </TableContainer>
       </Grid>
-      <div>
-         {openButton}
-         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>{"Add Task"}</DialogTitle>
-            <DialogContent>
-               {/* TODO: Feel free to change the properties of these components to implement editing functionality. The InputProps props class for these MUI components allows you to change their traditional CSS properties. */}
-               <TextField
-                  margin="normal"
-                  id="name"
-                  label="Name"
-                  fullWidth
-                  variant="standard"
-                  value={name}
-                  onChange={(event) => setName(event.target.value)}
-               />
-               <TextField
-                  margin="normal"
-                  id="description"
-                  label="Description"
-                  fullWidth
-                  variant="standard"
-                  multiline
-                  maxRows={8}
-                  value={description}
-                  onChange={(event) => setDescription(event.target.value)}
-               />
-               <TextField
-                  margin="normal"
-                  id="reward"
-                  label="Reward"
-                  fullWidth
-                  variant="standard"
-                  multiline
-                  maxRows={8}
-                  value={reward}
-                  onChange={(event) => setReward(event.target.value)}
-               />
-               <TextField
-                  margin="normal"
-                  id="due"
-                  label="Due"
-                  fullWidth
-                  variant="standard"
-                  multiline
-                  maxRows={8}
-                  value={due}
-                  onChange={(event) => setDue(event.target.value)}
-               />
-            </DialogContent>
-            {actionButtons}
-         </Dialog>
-      </div>
-    </>
    )
 }
