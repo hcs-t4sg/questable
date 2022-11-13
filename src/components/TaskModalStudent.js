2dc0411df65ec3a54221df45aadd8d48d2620084
Alex Estevez <aestevez@college.harvard.edu>
2022-11-13 18:59:29 -0500

    student view dashboard

--- src/components/TaskModalStudent.js
+++ src/components/TaskModalStudent.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+<<<<<<< HEAD
 import {
   Typography,
   Box,
@@ -39,6 +40,46 @@ export default function TaskModalStudent({ classroom, player, task }) {
       <Typography fontWeight="light" variant="h7">{data}</Typography>
     </>
   )
+=======
+import { 
+    Typography, 
+    Box, 
+    IconButton,
+    Modal, 
+    Button
+} from '@mui/material';
+import sprite1 from '../utils/tempAssets/sprite1.svg'
+import crossButton from '../utils/tempAssets/CrossButton.svg'
+import { completeTask } from '../utils/mutations';
+
+export default function TaskModalStudent({ classroom, player, task }) {
+    // State variables
+    const [open, setOpen] = useState(false);
+
+    // Open the task modal
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    // Close the task modal
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    // Handle task completion
+    const handleComplete = () => {
+        // Call the `completeTask` mutation
+        if(window.confirm("Are you sure you want to mark this task as complete?")) {
+          completeTask(classroom.id, task.id, player.id)
+        }
+    }
+
+    const Cluster = ({title, data}) => (
+      <>
+        <Typography sx={{marginTop: '25px'}} fontWeight="medium" variant="h7">{title}</Typography>
+        <Typography fontWeight="light" variant="h7">{data}</Typography>
+      </>
+    )
+>>>>>>> 84a559b (student view dashboard)
 
   const openButton =
     <IconButton onClick={handleClickOpen}>
@@ -47,6 +88,7 @@ export default function TaskModalStudent({ classroom, player, task }) {
 
   return (
     <div>
+<<<<<<< HEAD
       {openButton}
       <Modal sx={{ overflow: "scroll" }} open={open} onClose={handleClose}>
         <Box sx={{
@@ -102,6 +144,61 @@ export default function TaskModalStudent({ classroom, player, task }) {
             <Cluster title="Reward Amount" data={`$${task.reward}`} />
             {task.status === 'assigned' ? (<Cluster title="Completion" data={<Button onClick={handleComplete} variant="contained">Mark as complete</Button>} />)
               : <Cluster title="Completion" data={task.status === 'completed' ? 'Marked as completed!' : task.status === 'confirmed' ? 'Confirmed!' : 'Unavailable'} />
+=======
+       {openButton}
+       <Modal sx={{ overflow: "scroll"}} open={open} onClose={handleClose}>
+          <Box sx={{
+                position: 'absolute', 
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '70%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '35px',
+                paddingTop: '25px',
+                backgroundColor: 'white',
+                marginBottom: '18px',
+              }}
+          >
+            <Box sx={{width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+              <Typography fontWeight='light' variant="h5">Overview</Typography>
+              <Box onClick={handleClose} component="img" src={crossButton}></Box>
+            </Box>
+
+            <hr 
+              style={{
+                backgroundColor: '#D9D9D9',
+                height: '1px',
+                borderWidth: '0px',
+                borderRadius: '5px',
+                width: '100%',
+                marginBottom: '35px'
+              }}
+            />          
+            <Box
+              component="img"
+              sx={{
+                width: '15%',
+                // height: '30%',
+                maxHeight: '200px',
+                maxWidth: '200px',
+                marginBottom: '24px'
+              }}
+              alt="User's avatar"
+              src={sprite1}
+            />
+          <Typography sx={{textAlign: 'center', maxWidth: '400px'}} fontWeight="light" variant="h6">Flavored Text: Strawberry Vanilla Chocolate!</Typography>
+          <Box sx={{width:'100%', flexDirection: 'column', display: 'flex', justifyContent: 'left'}}>
+            <Cluster title="Task Name" data={task.name}/>
+            <Cluster title="Description" data={task.description}/>
+            <Cluster title="Deadline" data={task.due}/>
+            <Cluster title="Reward Amount" data={`$${task.reward}`}/>
+            {task.status === 'assigned' ? (<Cluster title="Completion" data={<Button onClick={handleComplete} variant="contained">Mark as complete</Button>}/>)
+              : <Cluster title="Completion" data={task.status === 'completed' ? 'Marked as completed!' : task.status === 'confirmed' ? 'Confirmed!' : 'Unavailable'}/>
+>>>>>>> 84a559b (student view dashboard)
             }
           </Box>
         </Box>
