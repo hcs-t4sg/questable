65d0d0b413c9c05c93c0e2ad2e13dad493b87c04
Priscilla Wang <hjkim015@users.noreply.github.com>
2022-11-19 20:32:51 -0500

    displayed singular avatar

--- src/routes/student/Inventory.js
+++ src/routes/student/Inventory.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext, useReducer } from "react"
 // import ReactDOM from "react-dom"
 import Spritesheet from "react-responsive-spritesheet"
+import Player from "./Player"
 // import debounce from 'lodash.debounce'
 // import { keybindings } from './keys'
 // import { KeysContext } from './index'
@@ -8,28 +9,37 @@ import Spritesheet from "react-responsive-spritesheet"
 // import { css } from "emotion"
 // import { keybindings } from "./keys"
 // import Player from "./Player"
+import avatar from '../../utils/spriteSheets/current/char4.png'
 
-
-let link = "'../../utils/spriteSheets/current/char4.png'"
-// const dlog = debounce((...args) => console.log(...args), 200)
 
 
 export default function Inventory() {
     return (
+
         <div>
-            <h1>Inventory</h1>
-          {/* <KeysContext.Provider value={keysDown}> */}
-            <Player />
-            <img src={link} alt="avatar_sprite"/>
-          {/* </KeysContext.Provider> */}
+             <Spritesheet
+                image={avatar}
+                widthFrame={32}
+                heightFrame={32}
+                steps={1}
+                fps={10}
+                loop={false}
+                startAt={0}
+                endAt={0}
+                isResponsive={true}
+                />
         </div>
+       
+
+        // <div>
+        //     <h1>Inventory</h1>
+        //   {/* <KeysContext.Provider value={keysDown}> */}
+        //     <Player />
+        //     <img src={avatar} alt="avatar_sprite"/>
+        //   {/* </KeysContext.Provider> */}
+        // </div>
       )
  }
-
-
- 
-//  ReactDOM.render(<App />, document.getElementById("root"))
-
 
 
  //Making the avatar display 
@@ -47,81 +57,7 @@ export default function Inventory() {
 //     left: 0;`
 
 
- let Player = () => {
-    // let keysDown = useContext(KeyrspsContext)
-    let [spritesheet, setSpritesheet] = useState(null)
-    let [x, setX] = useState(0)
-    let [y, setY] = useState(0)
-  
-    // useEffect(() => {
-    //   let intervalId = setInterval(() => {
-    //     // TODO must re-effect to get updated keysDown!!
-    //     // Maybe pressedKeys can be derived from keysDown??
-    //     // Or something can be memoized, memoized callback?
-    //     // How can I make a context aware effect?
-  
-    //     // purely derived from keysDown reducer state
-  
-    //     dlog(JSON.stringify(keysDown))
-    //     let pressedKeys = Object.entries(keysDown)
-    //       .filter(e => e[1])
-    //       .map(e => e[0])
-  
-    //     if (pressedKeys.length > 0) {
-    //       console.log({ pressedKeys })
-    //     }
-    //     // purely derived from keysDown reducer state + keybindings
-    //     let activesMap = Object.entries(keybindings).reduce((mapping, binding) => {
-    //       let bindingName = binding[0]
-    //       let isOn = !!pressedKeys.find(key => binding[1].includes(key))
-    //       // console.log(`Found ${bindingName} to be ${isOn} when ${JSON.stringify(pressedKeys)}`)
-    //       return {
-    //         ...mapping,
-    //         [bindingName]: isOn,
-    //       }
-    //     }, {})
-  
-    //     // the only actual action of this effect, from entirely derived data... hmmm
-    //     // make activesMap or the individual ones we care about the props to this component??
-    //     if (activesMap.moveDown) {
-    //       console.log('moving down')
-    //       setY(y + 10)
-    //     }
-    //   }, 500)
-  
-    //   return () => clearInterval(intervalId)
-    // })
-  
-    useEffect(() => {
-      if (!spritesheet) {
-        return
-      }
-  
-      let frames = animationMapping['left']
-      // Update spritesheet instance instead of remounting/etc
-      spritesheet.goToAndPlay(frames[0])
-      spritesheet.setStartAt(frames[0])
-      spritesheet.setEndAt(frames[1])
-    })
-  
-    let frames = animationMapping['left']
-    return (
-      <Spritesheet
-        // className={spriteClass}
-        style={{ left: `${x}px`, top: `${y}px` }}
-        image={link}
-        widthFrame={300}
-        heightFrame={300}
-        fps={6}
-        startAt={frames[0]}
-        endAt={frames[1]}
-        getInstance={spritesheet => {
-          setSpritesheet(spritesheet)
-        }}
-      />
-    )
-  }
-
+ 
 
 
 
