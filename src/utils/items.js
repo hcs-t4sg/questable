71a2b9af7d4693f66e7380c1b8121ee57c054764
Matthew Su <matthew.su.2019@gmail.com>
2022-12-16 02:55:42 -0500

    added documentation, richer name and description generators

--- src/utils/items.js
+++ src/utils/items.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext, useReducer } from "react"
 import Spritesheet from "react-responsive-spritesheet"
 import { Box, ThemeProvider, createTheme, zIndex } from '@mui/system';
+import _, { map } from 'underscore';
+
 import body from '../assets/spriteSheets/characters/char_all.png';
 
 import bob from 'src/assets/spriteSheets/hair/bob.png';
@@ -23,22 +25,45 @@ import shirt from '../assets/spriteSheets/clothes/basic.png';
 import pants from '../assets/spriteSheets/clothes/pants.png';
 import shoes from '../assets/spriteSheets/clothes/shoes.png';
 
+// Guide to classes in Javascript: https://dmitripavlutin.com/javascript-classes-complete-guide/#32-private-instance-fields
 
 
-// Do a class approach for each item type because otherwise we would need to link specific spritesheets to IDs which is hard. If we need to add generic item properties we can always add a superclass of Item. 
+// Arrays mapping item IDs to their respective colors for names/descriptions. Order important.
+// Note that in our local data structure, item ID, spritesheet location, and color all correspond.
+const clothingColors = ['black', 'dark blue', 'light blue', 'brown', 'dark green', 'light green', 'pink', 'purple', 'red', 'beige'];
+const hairColors = ['black', 'blonde', 'dark brown', 'light brown', 'ginger', 'dark green', 'light green', 'gray', 'light purple', 'blue', 'pink', 'purple', 'red', 'turquoise'];
+
+/* Class description for Body, Shirt, Pants, Shoes:
+
+Fields:
+- id: Integer starting from 0 that corresponds to the color of the item (see above mappings) and the spritesheet location of the item. Note that items across different types or subtypes may have the same id.
+- name: Item name to be displayed on frontend
+- description: Item description to be displayed on frontend
+- #spriteStart: A private field (not accessible outside class) for accessing the correct spritesheet location
+- type: Type of the item ('body', 'shirt', 'pants', 'shoes')
+
+Methods:
+- renderStatic(): Display an unmoving version of the item
+- renderAnimated(): Display an animated version of the item
+*/
+
+/*
+Class description for Hair:
+Same as above, but there is an additional 'subtype' field. This corresponds to the hairstyle. Again, items with different 'subtype' fields can have the same 'id'
+*/
 
 export class Body {
    id;
    name;
    description;
-   spriteStart;
+   #spriteStart;
    type = "body";
 
    constructor(id) {
       this.id = id;
       this.name = "Body" + id.toString();
       this.description = "A skin tone for your avatar!";
-      this.spriteStart = 8 * id + 1;
+      this.#spriteStart = 8 * id + 1;
    }
 
    renderStatic() {
@@ -54,8 +79,8 @@ export class Body {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart}
             isResponsive={true}
          />
       )
@@ -74,8 +99,8 @@ export class Body {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart + 7}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart + 7}
             isResponsive={true}
          />
       )
@@ -86,21 +111,20 @@ export class Hair {
    id;
    name;
    description;
-   spriteStart;
+   #spriteStart;
    type = "hair";
    subtype;
 
+   // Import object to allow the correct image import based on the subtype string. This is a static private field of the class
    static #imports = { bob, braids, buzzcut, curly, emo, extra_long_skirt, extra_long, french_curl, gentleman, long_straight_skirt, long_straight, midiwave, ponytail, spacebuns, wavy }
 
    constructor(id, subtype) {
       this.id = id;
-      this.name = "Hair" + id.toString();
-      this.description = "Hair for your avatar!";
-      this.spriteStart = 8 * id + 1;
+      this.name = _.capitalize(hairColors[id]) + this.subtype.replaceAll('_', ' ') + "hair";
+      this.description = _.capitalize(hairColors[id]) + this.subtype.replaceAll('_', ' ') + "hair for your avatar!";
+      this.#spriteStart = 8 * id + 1;
       this.subtype = subtype;
    }
-
-   // Figure out how to conditionally import hair files to use in render. Dynamic imports?
 
    renderStatic() {
       return (
@@ -115,8 +139,8 @@ export class Hair {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart}
             isResponsive={true}
          />
       )
@@ -135,8 +159,8 @@ export class Hair {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart + 7}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart + 7}
             isResponsive={true}
          />
       )
@@ -147,14 +171,14 @@ export class Shirt {
    id;
    name;
    description;
-   spriteStart;
+   #spriteStart;
    type = "shirt";
 
    constructor(id, subtype) {
       this.id = id;
-      this.name = "Shirt" + id.toString();
-      this.description = "A shirt for your avatar!";
-      this.spriteStart = 8 * id + 1;
+      this.name = _.capitalize(clothingColors[id]) + "shirt";
+      this.description = "A " + _.capitalize(clothingColors[id]) + " shirt for your avatar!";
+      this.#spriteStart = 8 * id + 1;
       this.subtype = subtype;
    }
 
@@ -171,8 +195,8 @@ export class Shirt {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart}
             isResponsive={true}
          />
       )
@@ -191,8 +215,8 @@ export class Shirt {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart + 7}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart + 7}
             isResponsive={true}
          />
       )
@@ -203,14 +227,14 @@ export class Pants {
    id;
    name;
    description;
-   spriteStart;
+   #spriteStart;
    type = "pants";
 
    constructor(id) {
       this.id = id;
-      this.name = "Pants" + id.toString();
-      this.description = "Pants for your avatar!";
-      this.spriteStart = 8 * id + 1;
+      this.name = _.capitalize(clothingColors[id]) + "pants";
+      this.description = _.capitalize(clothingColors[id]) + "pants for your avatar!";
+      this.#spriteStart = 8 * id + 1;
    }
 
    renderStatic() {
@@ -226,8 +250,8 @@ export class Pants {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart}
             isResponsive={true}
          />
       )
@@ -246,8 +270,8 @@ export class Pants {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart + 7}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart + 7}
             isResponsive={true}
          />
       )
@@ -258,14 +282,14 @@ export class Shoes {
    id;
    name;
    description;
-   spriteStart;
+   #spriteStart;
    type = "shoes";
 
    constructor(id) {
       this.id = id;
-      this.name = "Shoes" + id.toString();
-      this.description = "Shoes for your avatar!";
-      this.spriteStart = 8 * id + 1;
+      this.name = _.capitalize(clothingColors[id]) + "shoes";
+      this.description = _.capitalize(clothingColors[id]) + "shoes for your avatar!";
+      this.#spriteStart = 8 * id + 1;
    }
 
    renderStatic() {
@@ -281,8 +305,8 @@ export class Shoes {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart}
             isResponsive={true}
          />
       )
@@ -301,56 +325,59 @@ export class Shoes {
             heightFrame={32}
             fps={10}
             loop={true}
-            startAt={this.spriteStart}
-            endAt={this.spriteStart + 7}
+            startAt={this.#spriteStart}
+            endAt={this.#spriteStart + 7}
             isResponsive={true}
          />
       )
    }
 }
 
+// Functions to generate all items of a given type in the game (for use in Shop)
+
 export function getBodyItems() {
-   // Generate array from 0 to 7
-   const bodyList = [...Array(8).keys()];
-   const bodyItems = bodyList.map((index) => new Body(index));
+   // Generate id array from 0 to 7
+   const idList = [...Array(8).keys()];
+   const bodyItems = idList.map((id) => new Body(id));
 
    return bodyItems;
 }
 
 export function getHairItems() {
-   const hairList = [
+   const hairSubtypes = [
       'bob', 'braids', 'buzzcut', 'curly', 'emo', 'extra_long_skirt', 'extra_long', 'french_curl', 'gentleman', 'long_straight', 'long_straight_skirt', 'midiwave', 'ponytail', 'spacebuns', 'wavy'
    ];
 
-   const bigArray = hairList.map((subtype) => {
+   const bigArray = hairSubtypes.map((subtype) => {
 
-      const colorList = [...Array(14).keys()];
+      const idList = [...Array(14).keys()];
 
-      const hairColors = colorList.map((index) => new Hair(index, subtype));
+      const hairItems = idList.map((id) => new Hair(id, subtype));
 
-      return hairColors
+      return hairItems
    })
 
+   // Flatten the 2D array created from the nested maps
    return bigArray.flat()
 };
 
 export function getShirtItems() {
-   const shirtList = [...Array(10).keys()];
-   const shirtItems = shirtList.map((index) => new Shirt(index));
+   const idList = [...Array(10).keys()];
+   const shirtItems = idList.map((id) => new Shirt(id));
 
    return shirtItems;
 }
 
 export function getPantsItems() {
-   const pantsList = [...Array(10).keys()];
-   const pantsItems = pantsList.map((index) => new Pants(index));
+   const idList = [...Array(10).keys()];
+   const pantsItems = idList.map((id) => new Pants(id));
 
    return pantsItems;
 }
 
 export function getShoesItems() {
-   const shoesList = [...Array(10).keys()];
-   const shoesItems = shoesList.map((index) => new Shoes(index));
+   const idList = [...Array(10).keys()];
+   const shoesItems = idList.map((id) => new Shoes(id));
 
    return shoesItems;
 }
