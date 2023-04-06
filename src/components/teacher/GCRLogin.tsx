675b84933fa25cccaeda93825395827c4fe7ee50
Ashley Gong <ashleygong@college.harvard.edu>
2023-04-06 11:09:26 -0400

    Revert "tried google auth through firebase, not enabled right now? ask at swe sync i guess"
    This reverts commit fc4d13fb30e999d8f49d17f1b5751d4c9e4660a2.

--- src/components/teacher/GCRLogin.tsx
+++ src/components/teacher/GCRLogin.tsx
@@ -1,7 +1,5 @@
-import { Box, Button } from '@mui/material'
-// import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { auth } from '../../utils/firebase'
+import { Box } from '@mui/material'
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login'
 
 export const clientID = '104855113016-3noojc4bmbk66foqac9pjf44vm3lbnlr.apps.googleusercontent.com'
 
@@ -10,35 +8,24 @@ export const clientID = '104855113016-3noojc4bmbk66foqac9pjf44vm3lbnlr.apps.goog
 // or switch to newer https://www.npmjs.com/package/@react-oauth/google
 
 export default function GCRLogin() {
-	const provider = new GoogleAuthProvider()
-
-	const signInWithGoogle = () => {
-		signInWithPopup(auth, provider)
-			.then((result) => {
-				console.log(result)
-			})
-			.catch((error) => console.log(error))
+	const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+		console.log('Login success! res: ', res)
 	}
-	// const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-	// 	console.log('Login success! res: ', res)
-	// }
-	// const onFailure = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-	// 	console.log('Login failure! res: ', res)
-	// }
+
+	const onFailure = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+		console.log('Login failure! res: ', res)
+	}
+
 	return (
 		<Box>
-			<Button variant='contained' onClick={signInWithGoogle}>
-				Sign in with Google
-			</Button>
-
-			{/* <GoogleLogin
+			<GoogleLogin
 				clientId={clientID}
 				buttonText='Login with Google'
 				onSuccess={onSuccess}
 				onFailure={onFailure}
 				cookiePolicy={'single_host_origin'}
 				isSignedIn={true}
-			/> */}
+			/>
 		</Box>
 	)
 }
