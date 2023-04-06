fc4d13fb30e999d8f49d17f1b5751d4c9e4660a2
Ashley Gong <ashleygong@college.harvard.edu>
2023-04-06 02:14:40 -0400

    tried google auth through firebase, not enabled right now? ask at swe sync i guess

--- src/components/teacher/GCRLogin.tsx
+++ src/components/teacher/GCRLogin.tsx
@@ -1,5 +1,7 @@
-import { Box } from '@mui/material'
-import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login'
+import { Box, Button } from '@mui/material'
+// import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { auth } from '../../utils/firebase'
 
 export const clientID = '104855113016-3noojc4bmbk66foqac9pjf44vm3lbnlr.apps.googleusercontent.com'
 
@@ -8,24 +10,35 @@ export const clientID = '104855113016-3noojc4bmbk66foqac9pjf44vm3lbnlr.apps.goog
 // or switch to newer https://www.npmjs.com/package/@react-oauth/google
 
 export default function GCRLogin() {
-	const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-		console.log('Login success! res: ', res)
-	}
+	const provider = new GoogleAuthProvider()
 
-	const onFailure = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-		console.log('Login failure! res: ', res)
+	const signInWithGoogle = () => {
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				console.log(result)
+			})
+			.catch((error) => console.log(error))
 	}
-
+	// const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+	// 	console.log('Login success! res: ', res)
+	// }
+	// const onFailure = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+	// 	console.log('Login failure! res: ', res)
+	// }
 	return (
 		<Box>
-			<GoogleLogin
+			<Button variant='contained' onClick={signInWithGoogle}>
+				Sign in with Google
+			</Button>
+
+			{/* <GoogleLogin
 				clientId={clientID}
 				buttonText='Login with Google'
 				onSuccess={onSuccess}
 				onFailure={onFailure}
 				cookiePolicy={'single_host_origin'}
 				isSignedIn={true}
-			/>
+			/> */}
 		</Box>
 	)
 }
