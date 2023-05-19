2d1acd3688d3cab428647d6ba80dcc433984d8bc
Matthew Su <matthewsu@college.harvard.edu>
2023-05-18 19:14:40 -0500

    Uncommented deprecated files and added deprecated to prettierignore

--- deprecated/GCRAPI.tsx
+++ deprecated/GCRAPI.tsx
@@ -1,25 +1,25 @@
-// ! NOT USING LOADCLIENT AT THE MOMENT BECAUSE GAPI AUTH2 IS OUTDATED
-// import { gapi } from 'gapi-script'
+! NOT USING LOADCLIENT AT THE MOMENT BECAUSE GAPI AUTH2 IS OUTDATED
+import { gapi } from 'gapi-script'
 
-// export function loadClient() {
-// 	gapi.load('client:auth2', () => {
-// 		console.log('loaded gapi client')
+export function loadClient() {
+	gapi.load('client:auth2', () => {
+		console.log('loaded gapi client')
 
-// 		gapi.client.init({
-// 			apiKey: API_KEY,
-// 			clientId: clientID,
-// 			discoveryDocs: ['https://classroom.googleapis.com/$discovery/rest?version=v1'],
-// 			scope: SCOPES,
-// 		})
+		gapi.client.init({
+			apiKey: API_KEY,
+			clientId: clientID,
+			discoveryDocs: ['https://classroom.googleapis.com/$discovery/rest?version=v1'],
+			scope: SCOPES,
+		})
 
-// 		gapi.client.load('classroom', 'v1', () => {
-// 			// now we can use gapi.client.classroom
-// 			console.log('gcr client loaded!')
-// 		})
-// 	})
+		gapi.client.load('classroom', 'v1', () => {
+			// now we can use gapi.client.classroom
+			console.log('gcr client loaded!')
+		})
+	})
 
-// 	gapi.auth2.getAuthInstance().signOut()
-// 	if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
-// 		gapi.auth2.getAuthInstance().signIn()
-// 	}
-// }
+	gapi.auth2.getAuthInstance().signOut()
+	if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
+		gapi.auth2.getAuthInstance().signIn()
+	}
+}
